2e47eb3160573e2f856331b717024c03bab32ec3
ilona <gogiashvili.ilona@gmail.com>
2024-04-15 12:20:04 -0400

    commit tree 8aba989c9f4e086c0902d93a6f83d4d6533c74bc

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Code your solution in this file!
+let drivers = ["Antonia", "Nuru", "Amari", "Mo"];
+const returnFirstTwoDrivers = function (array) {
+  return array.slice(0, 2);
+};
+const returnLastTwoDrivers = function (array) {
+  return array.slice(-2);
+};
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+const createFareMultiplier = function (integer) {
+  return (fare) => integer * fare;
+};
+const fareDoubler = function (createFareMultiplier) {
+  return createFareMultiplier * 2;
+};
+const fareTripler = createFareMultiplier(3);
